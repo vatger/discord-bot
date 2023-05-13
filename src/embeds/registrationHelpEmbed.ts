db5dee73562acf15b10abe712e86c5f07c3451df
Moritz <moritz.frenken@gmail.com>
2023-05-13 10:12:57 +0200

    updated registration help

--- src/embeds/registrationHelpEmbed.ts
+++ src/embeds/registrationHelpEmbed.ts
@@ -5,13 +5,12 @@ export const registrationHelpEmbed = new EmbedBuilder()
     .setTitle('Registration Help')
     .addFields([
         {
-            name: '**Why I see this channel?**',
-            value: `When you join the server you usually get the VATSIM Member role without any delay, because you own a VATSIM ID.
-                    We created some more channels for our user who are also members of VATSIM Germany. If you either don't be a member of VATSIM Germany or something was broken during your intiial role assignment, you will see this channel.`,
+            name: '**Why do I see this channel?**',
+            value: `As soon as you enter the server, you immediately get the VATSIM Member role, because you have a VATSIM ID.\nAs a member of VATSIM Germany you are able to see even more channels on this server.\nYou see this channel because you were not given the VATGER Member role in the beginning.\nThis can be either because you are not a member of VATSIM Germany or there was an error in the assignment of roles.\n\n`,
         },
         {
             name: '**How to get the VATGER-Member role?**',
-            value: `If you think you didn't get the VATGER Member role by mistake or you registered your VATSIM Account with the Germany Divsion afterwards, simply type \`/register\` in the chatbox to start the role assignment flow manually.`,
+            value: `If there was an error in assigning the role or you joined VATSIM Germany at a later time, just use the \`/register\` command to apply for the role again.`,
         },
         
     ]);
