5e3472c08ea21e145faf1ab9856b1cd125eff802
Moritz <moritz.frenken@gmail.com>
2023-11-11 21:50:27 +0100

    add vatsim ratings, removed note and warn

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -43,107 +43,6 @@ async function addUser(user: User, cid?: Number): Promise<UserDocument> {
     return _user;
 }
 
-async function warnUser(author: User, user: User, reason: string | null) {
-    try {
-        const _user = await userModel.findOneAndUpdate(
-            { discordId: user.id },
-            {
-                $push: {
-                    warnings: [
-                        {
-                            authorDiscordId: author.id,
-                            reason: reason ?? '',
-                        },
-                    ],
-                },
-            },
-            { upsert: true }
-        );
-    } catch (e: any) {
-        await sendBotLogMessage('Failed to add warning to User', e.message);
-    }
-}
-
-async function deleteWarn(user: User, warn_id: string) {
-    try {
-        const _user = await userModel.findOneAndUpdate(
-            { discordId: user.id },
-            {
-                $pull: {
-                    warnings: {
-                        _id: warn_id,
-                    },
-                },
-            },
-            { returnOriginal: true }
-        );
-
-        return _user;
-    } catch (e: any) {
-        await sendBotLogMessage(
-            'Failed to remove warning from User',
-            e.message
-        );
-        return undefined;
-    }
-}
-
-async function noteUser(author: User, user: User, message: string | null) {
-    try {
-        const _user = await userModel.findOneAndUpdate(
-            { discordId: user.id },
-            {
-                $push: {
-                    notes: [
-                        {
-                            authorDiscordId: author.id,
-                            message: message ?? '',
-                        },
-                    ],
-                },
-            },
-            { upsert: true }
-        );
-    } catch (e: any) {
-        await sendBotLogMessage('Failed to add note to User', e.message);
-    }
-}
-
-async function getUserWarnings(user: User) {
-    try {
-        const _user: UserDocument | null = await userModel.findOne({
-            discordId: user.id,
-        });
-
-        if (!_user) {
-            throw new Error('No User found');
-        }
-
-        return _user.warnings;
-    } catch (e: any) {
-        await sendBotLogMessage(
-            'Failed to retreive warning of User',
-            e.message
-        );
-    }
-}
-
-async function getUserNotes(user: User) {
-    try {
-        const _user: UserDocument | null = await userModel.findOne({
-            discordId: user.id,
-        });
-
-        if (!_user) {
-            throw new Error('No User found');
-        }
-
-        return _user.notes;
-    } catch (e: any) {
-        await sendBotLogMessage('Failed to retreive notes of User', e.message);
-    }
-}
-
 async function checkIsVatger(discordId: string) {
     const _user = await userModel.findOne({
         discordId: discordId
@@ -174,11 +73,6 @@ async function checkIsVatger(discordId: string) {
 }
 
 export default {
-    warnUser,
-    deleteWarn,
-    noteUser,
-    getUserNotes,
-    getUserWarnings,
     getAllUsers,
     addUser,
     updateCid,
