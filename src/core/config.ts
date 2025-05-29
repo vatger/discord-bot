9e15c3d618825ce4418d71543fe1afbb9ff3a5b5
Moritz <moritz.frenken@gmail.com>
2025-05-29 17:01:07 +0200

    fix

--- src/core/config.ts
+++ src/core/config.ts
@@ -72,7 +72,7 @@ export const Config: EnvConfig = {
     HP_TOKEN: process.env.HP_TOKEN ?? '',
 
     PING_GROUPS: ['EDDH', 'EDDB', 'EDDV', 'EDDL', 'EDDK', 'EDDF', 'EDDS', 'EDDP', 'EDDN', 'EDDM', 'CTR EDWW', 'CTR EDGG', 'CTR EDMM', 'Minor EDWW', 'Minor EDGG', 'Minor EDMM', 'ECFMP EDWW', 'ECFMP EDGG', 'ECFMP EDMM'],
-    MANAGEABLE_GROUPS: ['EDWW Mentor', 'EDGG Mentor', 'EDMM Mentor', 'S1 Mentor', 'Mentor', 'EDWW Nav', 'EDGG Nav', 'EDMM Nav', 'NAV', 'EDWW Event', 'EDGG Event', 'EDMM Event', 'Event', 'PTD Trainer', 'PMP Mentor','EDGG Leitung','EDMM Leitung','EDWW Leitung','Tech Leitung','VATGER Leitung','NAV Leitung','PTD Leitung','PR & Event Leitung','ATD Leitung'],
+    MANAGEABLE_GROUPS: ['EDWW Mentor', 'EDGG Mentor', 'EDMM Mentor', 'S1 Mentor', 'Mentor', 'EDWW Nav', 'EDGG Nav', 'EDMM Nav', 'NAV', 'EDWW Event', 'EDGG Event', 'EDMM Event', 'Event', 'PTD Trainer', 'PMP Mentor','EDGG Leitung','EDMM Leitung','EDWW Leitung','Tech Leitung','VATGER Leitung','NAV Leitung','PTD Leitung','PR & Event Leitung','ATD Leitung', 'Tech Leitung', 'Tech Team', 'VATGER Leitung'],
 
     EVENT_UPDATE: process.env.EVENT_UPDATE == 'true',
     EVENT_UPDATE_CRON: process.env.EVENT_UPDATE_CRON ?? '0 */3 0 0 0',
